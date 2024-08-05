f2d312c8970d5ae9cad91f816baa9735dd29e034
yeyeyey <cjttkfkd3941@gmail.com>
2024-08-05 13:54:31 +0900

    day22 middle_update

---
+++ day22/unemployed_community_back_end/models/user.js
@@ -0,0 +1,71 @@
+const Sequelize = require("sequelize");
+
+class User extends Sequelize.Model {
+  static initiate(sequelize) {
+    User.init(
+      {
+        email: {
+          type: Sequelize.STRING(40),
+          allowNull: true,
+          unique: true,
+        },
+        nick: {
+          type: Sequelize.STRING(15),
+          // allowNull: false,
+        },
+        password: {
+          type: Sequelize.STRING(100), // 여기서 password 부분에서 sha처리 한 값을 받는다면, 그 사이즈에 맞게 보관하면 되나?
+          allowNull: true,
+        },
+        provider: {
+          type: Sequelize.ENUM("local", "kakao"),
+          allowNull: false,
+        },
+        snsId: {
+          type: Sequelize.STRING(255),
+        },
+        name: {
+          type: Sequelize.STRING(20),
+          // allowNull: false,
+          unique: true,
+        },
+        age: {
+          type: Sequelize.INTEGER.UNSIGNED,
+          // allowNull: false,
+        },
+        married: {
+          type: Sequelize.BOOLEAN,
+          // allowNull: false,
+        },
+        comment: {
+          type: Sequelize.TEXT,
+          allowNul: false,
+        },
+      },
+      {
+        sequelize,
+        timestamps: true,
+        modelName: "User",
+        tableName: "users",
+        paranoid: true,
+        charset: "utf8",
+        collate: "utf8_general_ci",
+      }
+    );
+  }
+  static associate(db) {
+    db.User.hasMany(db.Post);
+    db.User.belongsToMany(db.User, {
+      foreignKey: "followingId",
+      as: "Followers",
+      through: "Follow",
+    });
+    db.User.belongsToMany(db.User, {
+      foreignKey: "followerId",
+      as: "Followings",
+      through: "Follow",
+    });
+  }
+}
+
+module.exports = User;
